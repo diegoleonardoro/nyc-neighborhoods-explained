0d4bcdef02661c2c699b7932578d69e21e277483
diegoleonardoro <47146321+diegoleonardoro@users.noreply.github.com>
2022-05-25 15:59:02 -0400

    make it better

--- server.js
+++ server.js
@@ -2,20 +2,20 @@ const express = require("express");
 
 const app = express();
 
+const path = require("path");
+
 // import routes:
 const mainPage = require("./routes/main");
-
 app.use("/data", mainPage);
 
-if (process.env.NOde_ENV === "production") {
-  app.use(express.static("client/build"));
+const PORT = process.env.PORT || 5000;
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("public"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    req.sendFile(path.resolve(__dirname, 'public', 'index.html' ));
   });
 }
-
-const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);

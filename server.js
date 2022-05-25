54070fc7f9e0a271326640c51db9aa6171a61fb6
diegoleonardoro <47146321+diegoleonardoro@users.noreply.github.com>
2022-05-25 13:18:46 -0400

    Initial deployment

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
-
-
 const express = require("express");
 
 const app = express();
 
 // import routes:
-const mainPage = require('./routes/main');
+const mainPage = require("./routes/main");
 
-app.use('/data', mainPage);
+app.use("/data", mainPage);
+
+if (process.env.NOde_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
-
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
